7201680b6177704dba82e85f39ce3d3588fcdc97
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2025-04-22 18:28:17 +0200

    Complex number

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -1,5 +1,7 @@
 package tasks.adts
 
+import tasks.adts.Ex1ComplexNumbers.BasicComplexADT.SequenceImpl
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -18,13 +20,16 @@ object Ex1ComplexNumbers:
       def asString(): String
 
   object BasicComplexADT extends ComplexADT:
+    case class SequenceImpl(real: Double, immaginary: Double)
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = SequenceImpl
+    def complex(re: Double, im: Double): Complex = SequenceImpl(re,im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.real
+      def im(): Double = complex.immaginary
+      def sum(other: Complex): Complex =
+        SequenceImpl(complex.real + other.real, complex.immaginary + other.immaginary)
+      def subtract(other: Complex): Complex =
+        SequenceImpl(complex.real - other.real, complex.immaginary - other.immaginary)
+      def asString(): String = s"${complex.real} + ${complex.immaginary}i"
